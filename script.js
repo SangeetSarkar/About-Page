361757b52965e072e9e74011c56c70b76c848adf
Sangeet Sarkar <sangeetsarkar2002@gmail.com>
2022-12-09 18:04:30 +0530

    added click functionality on buttons

--- script.js
+++ script.js
@@ -1,40 +1,53 @@
-const content=document.querySelector('.container-content');
+window.onload =function(){
 
-const historyBtn=document.querySelector('body > div.content > div > div.button-container > button.history');
-const visionBtn=document.querySelector('body > div.content > div > div.button-container > button.vision');
-const goalsBtn=document.querySelector('body > div.content > div > div.button-container > button.goals');
+    const content=document.querySelector('.container-content');
+    
+    const historyBtn=document.querySelector('.history');
+    const visionBtn=document.querySelector('.vision');
+    const goalsBtn=document.querySelector('.goals');
+    
+    let isHistorySet=true,isVisionSet=false,isGoalsSet=false;
 
-let isHistorySet=true,isVisionSet=false,isGoalsSet=false;
+    let h="Historys";
+    let v="Visions";
+    let g="Goals";
+    
+    function displayHistory(){
+        if(isHistorySet==false){
+            content.innerHTML=h;
+            isHistorySet=true;
+            isVisionSet=false;
+            isGoalsSet=false
 
-let history="historys";
-let vision="visions";
-let goal="goals";
-
-function displayHistory(){
-    if(isHistorySet==false){
-        content.innerHTML=history;
-        isHistorySet=true;
-        isVisionSet=false;
-        isGoalsSet=false
+            historyBtn.style.backgroundColor='rgb(235, 221, 76)';
+            visionBtn.style.backgroundColor='#effaf6';
+            goalsBtn.style.backgroundColor='#effaf6';
+        }
     }
-}
-
-function displayVision(){
+    
+    function displayVision(){
     if(isVisionSet==false){
-        content.innerHTML=vision;
+        content.innerHTML=v;
         isHistorySet=false
         isVisionSet=true;
         isGoalsSet=false
+
+        historyBtn.style.backgroundColor='#effaf6';
+        visionBtn.style.backgroundColor='rgb(235, 221, 76)';
+        goalsBtn.style.backgroundColor='#effaf6';        
     }
 }
 
 function displayGoals(){
     if(isGoalsSet==false){
-        content.innerHTML=goal;
+        content.innerHTML=g;
         isHistorySet=false
         isVisionSet=false
         isGoalsSet=true;
-
+        
+        historyBtn.style.backgroundColor='#effaf6';
+        visionBtn.style.backgroundColor='#effaf6';
+        goalsBtn.style.backgroundColor='rgb(235, 221, 76)';
     }
 }
 
@@ -42,4 +55,5 @@ historyBtn.addEventListener('click',displayHistory);
 visionBtn.addEventListener('click',displayVision);
 goalsBtn.addEventListener('click',displayGoals);
 
+}
 
